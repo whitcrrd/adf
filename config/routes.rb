a3201ab000c6ffeb5bf804c458328f439b2be7b1
Scott Chiang <scott.chiang1@gmail.com>
2012-11-27 12:27:09 -0800

    added a users controller, added a users index view, assigned users#index to be root_url

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Adf::Application.routes.draw do
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
   match 'signout', to: 'sessions#destroy', as: 'signout'
+  
+  root to: "users#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
