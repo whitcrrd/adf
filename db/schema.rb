e2ee015cc9fe4ef98bfc2cfc21857d91691cd22c
Scott Chiang <scott.chiang1@gmail.com>
2012-11-27 12:35:27 -0800

    added fields needed for oauth to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121127195120) do
+ActiveRecord::Schema.define(:version => 20121127203354) do
 
   create_table "athletes", :force => true do |t|
     t.string   "first_name"
@@ -47,8 +47,12 @@ ActiveRecord::Schema.define(:version => 20121127195120) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
   end
 
 end
