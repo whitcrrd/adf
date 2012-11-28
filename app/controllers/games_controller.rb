05f95fc2ad41e42e8dcc7be72a6b9ea280130f78
Scott Chiang <scott.chiang1@gmail.com>
2012-11-28 14:19:35 -0800

    added new, create, and show action to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,20 @@
 class GamesController < ApplicationController
+  def new
+    @game = Game.new
+    redirect_to @game
+  end
   
+  def create
+    @game = Game.new(params[:game])
+    if @game.save
+      redirect_to @game
+    else
+      redirect_to :back, notice: "Error creating a new game"
+    end
+  end
+  
+  def show
+    @game = Game.find(params[:id])
+    redirect_to @game
+  end
 end
