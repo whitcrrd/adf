10ca341c2841a254db8778cf020784e896044e60
Scott Chiang <scott.chiang1@gmail.com>
2012-11-28 14:40:56 -0800

    added comment to game_winner method in game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,16 +3,17 @@ class Game < ActiveRecord::Base
 
   has_many :teams
   
+  # Add @game.game_winner to game show view to display the winner of the game
   def game_winner
     find_teams
     if @home_team.points > @away_team.points
       self.winner_id = self.home_team_id
       self.loser_id = self.away_team_id
-      @home_team
+      @home_team.name
     else
       self.winner_id = self.away_team_id
       self.loser_id = self.home_team_id
-      @away_team
+      @away_team.name
     end
   end
   
