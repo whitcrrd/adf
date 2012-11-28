aa7349572ffbadda17cd1ca3c2ade77be6991ba0
Scott Chiang <scott.chiang1@gmail.com>
2012-11-28 14:34:37 -0800

    added name field to teams table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121128205923) do
+ActiveRecord::Schema.define(:version => 20121128223310) do
 
   create_table "athletes", :force => true do |t|
     t.string   "first_name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20121128205923) do
     t.integer  "points"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "name"
   end
 
   create_table "users", :force => true do |t|
