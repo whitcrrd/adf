767b4869752b4e7cf4a6dcdf606936f22fd5ccc0
Kunal Bhatt <bhattk09@gmail.com>
2012-11-28 20:51:32 -0800

    Added Jquery to sort by position

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -2,11 +2,19 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-// $(document).ready( function() {
-//   $(':checkbox').change( function() {
-//     var selected_athletes = [];
-//     $(':checked').each(function(index){selected_athletes.push(($(this).attr('id')));});
-//   });
-//
-//
-// });
+$(document).ready( function() {
+  $('position_PG').show();
+  $('.position_C').hide();
+  $('.position_SG').hide();
+  $('.position_SF').hide();
+  $('.position_PF').hide();
+  $('#position_list').on('change', function(){
+    console.log(event)
+    $('.position_PG').hide();
+    $('.position_C').hide();
+    $('.position_SG').hide();
+    $('.position_SF').hide();
+    $('.position_PF').hide();
+    $('.position_' + event.target.value).show();
+  });
+});
