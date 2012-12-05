7adba172816a639131c0193ee09988097c751bbc
dbc-apprentice <apprentice@devbootcamp.com>
2012-12-04 19:09:30 -0800

    removed unused code in team.js and removed unused partials/views

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -5,18 +5,22 @@ var Team = {
     $('.athletes-list').on('ajax:success', 'a.add-athlete', Team.addAthlete);
   },
 
-  showByPosition: function(){
-    if (event.target.value === "ALL") {
+  showByPosition: function(event){
+    var filter = event.target.value;
+
+    if (filter === "ALL") {
       $('[class^=position_]').show();
     } else {
       $('.position_PG, .position_C, .position_SG, .position_SF, .position_PF').hide();
-      $('.position_' + event.target.value).show();
+      $('.position_' + filter).show();
     }
   },
 
   removeAthlete: function(event, data) {
-    var play_id = $(this).closest('.player_card').attr("id");
-    $(this).closest('.player_card').remove();
+    var $card = $(this).closest('.player_card');
+    var play_id = $card.attr("id");
+    // var play_id = $(this).closest('.player_card').attr("id");
+    $card.remove();
     $('#' + play_id + '_tr').show();
 
   },
@@ -29,48 +33,3 @@ var Team = {
 };
 
 $(document).ready(function() { Team.init(); });
-
-jQuery.fn.sortElements = (function(){
-
-    var sort = [].sort;
-
-    return function(comparator, getSortable) {
-
-        getSortable = getSortable || function(){return this;};
-
-        var placements = this.map(function(){
-
-            var sortElement = getSortable.call(this),
-                parentNode = sortElement.parentNode,
-                nextSibling = parentNode.insertBefore(
-                    document.createTextNode(''),
-                    sortElement.nextSibling
-                );
-
-            return function() {
-
-                if (parentNode === this) {
-                    throw new Error(
-                        "You can't sort elements if any one is a descendant of another."
-                    );
-                }
-                parentNode.insertBefore(this, nextSibling);
-                parentNode.removeChild(nextSibling);
-            };
-
-        });
-        return sort.call(this, comparator).each(function(i){
-            placements[i].call(getSortable.call(this));
-        });
-    };
-
-})();
-
-// $document.ready(function(){
-
-
-// });
-
-// $('.player_table #players').sortElements(function(a, b){
-//     return $(a).text() > $(b).text() ? 1 : -1;
-// });
