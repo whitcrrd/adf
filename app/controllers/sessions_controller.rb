97cee8723470e5a2a1b22a82dc01f2bc7e1a8bbd
DBC Apprentice <apprentice@devbootcamp.com>
2012-12-05 15:54:44 -0800

    added sessions request location so when a user gets denied access to a page, gets redirected to home page, and logs in, the user gets redirected to the first page it was trying to access

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,10 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url
+    redirect_back_or root_path
+    # redirect_to root_url
   end
-  
+
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "You have successfully logged out"
